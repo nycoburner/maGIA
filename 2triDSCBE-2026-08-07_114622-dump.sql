4db05ad49f472ad6c8e11b84323999b4ef683824
Nyco's_burner <nycolas.galvao@escola.pr.gov.br>
2026-08-12 09:44:45 -0300

    Update 2triDSCBE-2026-08-07_114622-dump.sql

--- 2triDSCBE-2026-08-07_114622-dump.sql
+++ 2triDSCBE-2026-08-07_114622-dump.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: 2triDSCBE
--- ------------------------------------------------------
--- Server version	8.0.34-0ubuntu0.20.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Cliente`
---
 
 DROP TABLE IF EXISTS `Cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Cliente` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(50) NOT NULL,
@@ -33,25 +12,20 @@ CREATE TABLE `Cliente` (
   UNIQUE KEY `CPF` (`CPF`),
   UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Cliente`
---
 
 LOCK TABLES `Cliente` WRITE;
-/*!40000 ALTER TABLE `Cliente` DISABLE KEYS */;
+
 INSERT INTO `Cliente` VALUES (1,'Nycolas','888.888.888.88','nycolas.galvao@GMAIL','#Perola21%','(42)99999-4444'),(5,'Bridget','324.858.878.94','bridget.ligoski@gmail.com','#Unika23%','(42)99934-9588'),(6,'Unika','454.678.323.74','Unika.lima@gmail.com','#Bridget22%','(42)99942-4537'),(7,'Rosa','454.378.334.21','rosa.ligoski@gmail.com','#Esmeralda20%','(42)99233-4111'),(8,'Eduardo','333.374.534.21','Eduardo.ligoski@gmail.com','#Esmeralda20%','(42)23333-4121'),(9,'Alauanny','312.554.568.56','Alauanny.lima@gmail.com','#Assas20%','(42)2455-4661'),(10,'Flavio','222.154.548.66','Flavio.ligoski@gmail.com','3432MR%','(42)2345-6334'),(11,'Pietro','442.133.456.05','pietrinho.raivoso@gmail.com','Ruvinhoscomestilo','(42)43587-6900'),(12,'Guilherme','772.213.456.67','guilherme.luiz@gmail.com','meajudaaaaaaaaa','(42)12557-6344'),(13,'Richard','442.richard.12','richard.promarx@gmail.com','novingas-e-veinhas','(42)Richa-1214');
-/*!40000 ALTER TABLE `Cliente` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SELECT Email,Senha from Cliente
+WHERE * Email =  'bridget.ligoski@gmail.com';
 
--- Dump completed on 2026-08-07 11:46:22
+SELECT * FROM Cliente WHERE id < 6;
+
+SELECT * FROM Cliente WHERE id >= 3 AND LENGTH(Senha) <20;
+
+DELETE FROM Cliente WHERE id = 10
+
+UPDATE Cliente SET nome = 'Richardinho', Email = 'Richard.pro.MAX@veinhas' WHERE id ='13';
